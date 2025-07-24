b4ba043754208873a513e12f860e0ca45f394f2f
ajithsunkara96 <ajithsunkara@gmail.com>
2025-07-24 15:28:10 -0400

    :Updated health message in conflict branch

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,18 @@
 from flask import Flask, jsonify
 import random
+
 app = Flask(__name__)
+
 @app.route('/health')
 def health():
-return jsonify({"status": "SYSTEM HEALTH OK"})
+    return jsonify({"status": "SYSTEM HEALTH OK - ALL SYSTEMS GO"})
+
 @app.route('/metrics')
 def metrics():
-return jsonify({
-"cpu": random.randint(1, 100),
-"memory": random.randint(1, 100)
-})
+    return jsonify({
+        "cpu": random.randint(1, 100),
+        "memory": random.randint(1, 100)
+    })
+
 if __name__ == '__main__':
-app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
